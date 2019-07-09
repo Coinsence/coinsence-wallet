adf705d997fbe481895f7fa2e34b429300215529
Haythem Sellami <sellamihaythem96@gmail.com>
2019-07-09 16:19:29 +0100

    scan wallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+
+  rootPage: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +16,16 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      //if an imported, scanned or created wallet exist
+      alert(localStorage.getItem("isWallet"));
+      if(localStorage.getItem("isWallet") == "true") {
+        this.rootPage = 'HomePage';
+      }
+      else {
+        this.rootPage = 'LoginPage';
+      }
+
     });
   }
 }
