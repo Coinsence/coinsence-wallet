acc568b4dd8d4e6825c263dfea633cd64c108848
Haythem Sellami <haythem96@users.noreply.github.com>
2019-07-30 17:18:18 +0100

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  apiEtherscanRinkeby: {
+    url: 'https://api-rinkeby.etherscan.io/api',
+    apiKey: ''
+  },
+  apiEthplorerMainnet: {
+    url: 'http://api.ethplorer.io',
+    apiKey: ''
+  },
+  apiBlockscoutRinkeby: {
+    url: 'http://blockscout.com/eth/rinkeby/api',
+    apiKey: ''
+  }
+}
