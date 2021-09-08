19768fd16b32f9102f75fbec9c9c58fc66a3cc3d
Haythem Sellami <sellamihaythem96@gmail.com>
2021-09-08 22:52:45 +0100

    update default tokens

--- src/utils/default-tokens.js
+++ src/utils/default-tokens.js
@@ -1,9 +1,15 @@
 const defaultTokens = [
   {
-    contractAddress: "0xb705b833b2A6413e778c45A4499EE1c048875BF5",
-    name: "Coinsence Community Coin",
-    symbol: "CCC",
-    decimals: 0
+    contractAddress: "0xefbc4C762698F2145A55F14e2E282BBc35B725F7",
+    name: "Tunisia Impact Coin",
+    symbol: "TIC",
+    decimals: 18
+  },
+  {
+    contractAddress: "0xBA92b5e4Fd50f2C500aE4d000C7D327C6381313d",
+    name: "Tunisia Dinar Coin",
+    symbol: "TND",
+    decimals: 18
   }
 ];
 
